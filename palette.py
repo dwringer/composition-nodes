e68d4cfd0b09235346db0828c0dd9e5807949c4c
Darren Ringer <dwringer@gmail.com>
2024-07-21 23:20:10 -0400

    update to allow larger maps and more control of sample size

--- palette.py
+++ palette.py
@@ -21,6 +21,7 @@ from invokeai.invocation_api import (
     invocation_output,
 )
 
+
 def tensor_from_pil_image(img, normalize=False):
     return image_resized_to_grid_as_tensor(img, normalize=normalize, multiple_of=1)
 
@@ -160,9 +161,9 @@ class ImageSOMOutput(BaseInvocationOutput):
 @invocation(
     'image_som',
     title="Image Quantize (Kohonen map)",
-    tags=["image", "quantize", "som", "kohonen", "palette"],
+    tags=["image", "color", "quantize", "som", "kohonen", "palette"],
     category="image",
-    version="0.4.2"
+    version="0.6.2"
 )
 class ImageSOMInvocation(BaseInvocation):
     """Use a Kohonen self-organizing map to quantize the pixel values of an image"""
@@ -171,28 +172,33 @@ class ImageSOMInvocation(BaseInvocation):
         default=None,
         description="Use an existing SOM instead of training one (skips all training)"
     )
-    map_width:  int = InputField(default=8,   description="Width (in cells) of the self-organizing map to train")
-    map_height: int = InputField(default=3,   description="Height (in cells) of the self-organizing map to train")
-    steps:  int = InputField(default=30, description="Training step count for the self-organizing map")
-    train_image_width: Optional[int] = InputField(default=128, description="Optional width to scale training image")
-    train_image_height: Optional[int] = InputField(default=128, description="Optional height to scale training image")
+    map_width:  int = InputField(default=16,   description="Width (in cells) of the self-organizing map to train")
+    map_height: int = InputField(default=16,   description="Height (in cells) of the self-organizing map to train")
+    steps:  int = InputField(default=128, description="Training step count for the self-organizing map")
+    training_scale: float = InputField(default=0.25, description="Nearest-neighbor scale image size prior to sampling")
+    sample_width: Optional[int] = InputField(default=64, description="Width of randomly selected sample per step")
+    sample_height: Optional[int] = InputField(default=64, description="Height of randomly selected sample per step")
 
 
     def invoke(self, context: InvocationContext) -> ImageSOMOutput:
         image_in = None
+
         if self.map_in is not None:
             image_in = context.images.get_pil(self.map_in.image_name)
         else:
             image_in = context.images.get_pil(self.image_in.image_name)
 
-            if self.train_image_width is not None:
-                if self.train_image_height is not None:
-                    image_in = image_in.resize((self.train_image_width, self.train_image_height), Image.NEAREST)
-                else:
-                    image_in = image_in.resize((image_in.width, self.train_image_height), Image.NEAREST)
-            else:
-                if self.train_image_height is not None:
-                    image_in = image_in.resize((self.train_image_width, image_in.height), Image.NEAREST)
+            if not (self.training_scale == 1.0):
+                image_in = image_in.resize(
+                    (int(image_in.width*self.training_scale), int(image_in.height*self.training_scale)),
+                    Image.NEAREST
+                )
+
+            if (image_in.width * image_in.height) < (self.sample_width * self.sample_height):
+                image_in = image_in.resize(
+                    (self.sample_width, self.sample_height),
+                    Image.NEAREST
+                )
 
         image_in = image_in.convert('RGB')
         image_in = tensor_from_pil_image(image_in, normalize=False)
@@ -220,7 +226,9 @@ class ImageSOMInvocation(BaseInvocation):
             sample_indices = torch.randperm(image_in.shape[2] * image_in.shape[3])
             sample = image_in.view(1, 3, -1)[:, :, sample_indices]
 
-            init_sample = sample[:, :, :(self.map_width * self.map_height)].view(1, 3, self.map_height, self.map_width)
+            init_sample = sample[:, :, :(self.map_width * self.map_height)].view(
+                1, 3, self.map_height, self.map_width
+            )
 
             som_tensor = torch.clone(init_sample)
             row_indices = torch.arange(
@@ -235,14 +243,17 @@ class ImageSOMInvocation(BaseInvocation):
 
             for i in range(self.steps):
                 sample_indices = torch.randperm(image_in.shape[2] * image_in.shape[3])
-                sample = image_in.view(1, 3, -1)[:, :, sample_indices].view(image_in.shape)
+                sample = image_in.view(1, 3, -1)[:, :, sample_indices]
+                sample = sample[:, :, :int(self.sample_height*self.sample_width)].view(
+                    1, 3, self.sample_height, self.sample_width
+                )
 
                 neighborhood_width = neighborhood_width_max - (
                     neighborhood_width_max * float(i) / self.steps
                 )
 
-                for j in range(image_in.shape[2]):
-                    for k in range(image_in.shape[3]):
+                for j in range(sample.shape[2]):
+                    for k in range(sample.shape[3]):
                         image_pixel = sample[:, :, j, k].expand(
                             self.map_height,
                             self.map_width,
@@ -278,7 +289,7 @@ class ImageSOMInvocation(BaseInvocation):
                             )
                         )
                     
-        if (self.map_in is not None) or (self.train_image_width is not None) or (self.train_image_height is not None):
+        if (self.map_in is not None) or (not (self.training_scale == 1.0)):
             image_in = context.images.get_pil(self.image_in.image_name)
             image_in = image_in.convert('RGB')
             image_in = tensor_from_pil_image(image_in, normalize=False)
@@ -286,26 +297,58 @@ class ImageSOMInvocation(BaseInvocation):
 
         image_out = torch.clone(image_in)
 
-        # For every pair of som coordinates, this contains the entire image:
-        _img = image_out.expand(*(list(som_tensor.shape[-2:])+list(image_out.shape))).movedim(0,5).movedim(0,5)
-        # For every pair of image coordinates, this contains the entire SOM:
-        _som = som_tensor.expand(*(list(image_out.shape[-2:])+list(som_tensor.shape))).movedim(0,3).movedim(0,3)
-        # 1, 3, img_h, img_w, som_h, som_w
+        if (  # Do the entire computation at once if the map is small enough
+                (som_tensor.shape[2] * som_tensor.shape[3] * image_out.shape[2] * image_out.shape[3]) <=
+                (32 * 32 * 1920 * 1080)  # TODO: Need to determine limits...
+        ):
+            # For every pair of som coordinates, this contains the entire image:
+            image_pixels = image_out.expand(
+                *(list(som_tensor.shape[-2:])+list(image_out.shape))
+            ).movedim(0,5).movedim(0,5)
+            # For every pair of image coordinates, this contains the entire SOM:
+            som_pixels = som_tensor.expand(
+                *(list(image_out.shape[-2:])+list(som_tensor.shape))
+            ).movedim(0,3).movedim(0,3)
+            # 1, 3, img_h, img_w, som_h, som_w
 
-        # best matching units by least sums of squares of elementwise distances
-        _sos = torch.sum(torch.square(torch.sub(_som, _img)), 1)
-        _min_0, _idx_0 = torch.min(_sos, 3)
-        _min, _idx = torch.min(_min_0, 3)
-        # _min is (1, h, w) min distances, while _idx is (1, h, w) bmu indices
+            # best matching units by least sums of squares of elementwise distances
+            sos = torch.sum(torch.square(torch.sub(som_pixels, image_pixels)), 1)
+            som_min_i, som_idx_i = torch.min(sos, 3)
+            som_min_j, som_idx_j = torch.min(som_min_i, 3)
+            # _min is (1, h, w) min distances, while som_idx_j is (1, h, w) bmu indices
 
-        bmus_j = _idx[0]
-        bmus_i = torch.empty((image_out.shape[2], image_out.shape[3]), dtype=bmus_j.dtype)
-        
-        for i in range(image_in.shape[2]):
-            _row_bmus_j = _idx[0, i]
-            bmus_i[i] = _idx_0[0, i, torch.arange(_idx_0.shape[2]), _row_bmus_j]
+            bmus_j = som_idx_j[0]
+            bmus_i = torch.empty((image_out.shape[2], image_out.shape[3]), dtype=bmus_j.dtype)
 
-        image_out = som_tensor[:,:,bmus_i,bmus_j]
+            for i in range(image_in.shape[2]):
+                row_bmus_j = som_idx_j[0, i]
+                bmus_i[i] = som_idx_i[0, i, torch.arange(som_idx_i.shape[2]), row_bmus_j]
+
+            image_out = som_tensor[:,:,bmus_i,bmus_j]
+        else:
+            bmus_i = torch.empty((image_out.shape[2], image_out.shape[3]), dtype=torch.int32)
+            bmus_j = torch.empty((image_out.shape[2], image_out.shape[3]), dtype=torch.int32)
+            for i in range(image_out.shape[2]):
+
+                # For every pair of som coordinates, this contains the entire image row:
+                row_img_pixels = image_out[:,:,i,:].expand(
+                    *(list(som_tensor.shape[-2:])+list(image_out.shape[:2])+[image_out.shape[3]])
+                ).movedim(0,4).movedim(0,4)
+                # For every column coordinate of the image row, this contains the entire SOM:
+                row_som_pixels = som_tensor.expand(
+                    *([image_out.shape[3]]+list(som_tensor.shape))
+                ).movedim(0,2)
+
+                # best matching units by least sums of squares of elementwise distances
+                row_sos = torch.sum(torch.square(torch.sub(row_som_pixels, row_img_pixels)), 1)
+                row_som_min_i, row_som_idx_i = torch.min(row_sos, 2)
+                row_som_min_j, row_som_idx_j = torch.min(row_som_min_i, 2)
+
+                bmus_j[i] = row_som_idx_j[0]
+
+                bmus_i[i] = row_som_idx_i[0, torch.arange(row_som_idx_i.shape[1]), bmus_j[i]]
+
+            image_out = som_tensor[:,:,bmus_i,bmus_j]
 
         image_out = pil_image_from_tensor(image_out[0], mode="RGB")
         image_dto = context.images.save(image_out)
